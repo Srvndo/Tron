9b90986fa2630a444da75d8bc545eced060fef94
raul cordoba <repp0731@gmail.com>
2017-11-05 01:25:47 -0400

    Load Map on Cookiey

--- static/js/react/game-menu.jsx
+++ static/js/react/game-menu.jsx
@@ -1,8 +1,54 @@
+class LoadMap extends React.Component {
+    constructor (props) {
+        super(props)
+        $("#file-div").css("display", "flex")
+    }
+
+    render () {
+        return (
+            <button type="button" className="btn btn-primary" onClick={() => this.props.loaded(false)}>Load</button>
+        )
+    }
+}
+
 class Game extends React.Component {
     constructor (props) {
-        super(props);
-        this.startGame = this.startGame.bind(this);
-        this.startEdithor = this.startEdithor.bind(this);
+        super(props)
+        this.state = {
+            load: false
+        }
+        this.startGame = this.startGame.bind(this)
+        this.startEdithor = this.startEdithor.bind(this)
+        this.loadMap = this.loadMap.bind(this)
+        this.readFile = this.readFile.bind(this)
+    }
+
+    readFile (e) {
+        let file = e.target.files[0]
+        let reader = new FileReader()
+        reader.onload = (e) => {
+            let date = new Date()
+            date.setTime(date.getTime() + (24*60*60*1000))
+            const expires = "expires" + date.toUTCString()
+            document.cookie = "map" + "=" + e.target.result + ";" + expires + ";path=/"
+        }
+        reader.readAsText(file)
+
+        
+
+        /*var name = "map ="
+        var decodedCookie = decodeURIComponent(document.cookie)
+        var ca = decodedCookie.split(';')
+        for(var i = 0; i <ca.length; i++) {
+        var c = ca[i]
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            console.log (c.substring(name.length, c.length))
+        }
+    }
+    return "";*/
     }
 
     startGame () {
@@ -16,10 +62,19 @@ class Game extends React.Component {
         )
     }
 
+    loadMap () {
+        if (!status) {
+            $("#file-div").css("display", "none")
+        }
+        this.setState({
+            load: !this.state.load
+        })
+    }
+
     render  () {
         return(
             <div className="text-center">
-                <label style={{fontSize: "50px"}}>Tron</label>
+                <label style={{fontSize: "50px"}}><strong>Tron</strong></label>
                 <br />
                 <br />
                 <button className="btn btn-primary" onClick={this.startGame}>Start 1v1</button>
@@ -29,6 +84,18 @@ class Game extends React.Component {
                 <br />
                 <br />
                 <button className="btn btn-secondary" onClick={this.startEdithor}>Make Map</button>
+                <br />
+                <br />
+                <div id="load">
+                    {this.state.load ?
+                        <LoadMap loaded={this.loadMap}/>
+                        :
+                        <button className="btn btn-secondary" onClick={this.loadMap}>Load Map</button>
+                    }
+                </div>
+                <div id="file-div" style={{display: "none"}}>
+                    <input id="file" type="file" onChange={this.readFile}/>
+                </div>
             </div>
         )
     }
